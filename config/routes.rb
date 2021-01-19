c2578c2a304fc98945373278091f7da9896ba28b
YodaFang <yodafang0121@gmail.com>
2021-01-19 10:37:18 +0200

    init with vuetify

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'admin/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'admin#index'
+end
---
+++ config/webpack/environment.js
@@ -0,0 +1,37 @@
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+environment.module = {
+    rules: [
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            // Requires sass-loader@^7.0.0
+            options: {
+              implementation: require('sass'),
+              indentedSyntax: true // optional
+            },
+            // Requires sass-loader@^8.0.0
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                indentedSyntax: true // optional
+              },
+            },
+          },
+        ],
+      },
+    ],
+  }
+
+console.log('I am here..................')
+console.log(environment)
+module.exports = environment
